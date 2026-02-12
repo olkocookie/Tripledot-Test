c939129268e45b0513dbcb7f96e11fc58edd08ad
madcookie <olko.protska@gmail.com>
2026-02-12 15:35:04 +0100

    Further tweaks to animation in LevelComplete

--- Assets/Scripts/TestLevelComplete.cs
+++ Assets/Scripts/TestLevelComplete.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 public class TestLevelComplete : MonoBehaviour
 {
     [SerializeField] private Button testButton;
-    [SerializeField] private GameObject levelCompletedScreen; // Changed to GameObject
+    [SerializeField] private GameObject levelCompletedScreen;
 
     private void Start()
     {
